3111932eeb94697788a89e13eccdea5f7fee2665
avpetin <alex_beholder@mail.ru>
2023-11-14 14:25:45 +0300

    Update1

--- crud/servlets/src/main/java/ru/netology/controller/PostController.java
+++ crud/servlets/src/main/java/ru/netology/controller/PostController.java
@@ -11,15 +11,16 @@ import java.io.Reader;
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
+  private final Gson gson = new Gson();
 
   public PostController(PostService service) {
     this.service = service;
+
   }
 
   public void all(HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
     final var data = service.all();
-    final var gson = new Gson();
     response.getWriter().print(gson.toJson(data));
   }
 
@@ -29,7 +30,6 @@ public class PostController {
 
   public void save(Reader body, HttpServletResponse response) throws IOException {
     response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
     final var post = gson.fromJson(body, Post.class);
     final var data = service.save(post);
     response.getWriter().print(gson.toJson(data));
--- crud/servlets/src/main/java/ru/netology/repository/PostRepository.java
+++ crud/servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -2,24 +2,42 @@ package ru.netology.repository;
 
 import ru.netology.model.Post;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 // Stub
 public class PostRepository {
+  private final ConcurrentMap<Long, Post> postMap = new ConcurrentHashMap<>();
+  private long postId;
   public List<Post> all() {
-    return Collections.emptyList();
+    return postMap.values().parallelStream().toList();
   }
 
   public Optional<Post> getById(long id) {
-    return Optional.empty();
+    return Optional.ofNullable(postMap.get(id));
   }
 
   public Post save(Post post) {
+    if(post.getId() == 0){
+      post.setId(++postId);
+      postMap.put(postId, post);
+    }
+    else{
+      if(getById(post.getId()).isPresent()) {
+        postMap.replace(post.getId(), post);
+      }
+      else{
+        postMap.put(postId, post);
+      }
+    }
     return post;
   }
 
   public void removeById(long id) {
+    if(getById(id).isPresent()){
+      postMap.remove(id);
+    }
   }
 }
